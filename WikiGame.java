7156e5397b87b64eacb971dcbed29633006ad5ac
killiam-cs <113317511+killiam-cs@users.noreply.github.com>
2023-01-18 08:10:03 -0500

    a bit more skeleton code if you need help with the structure

--- WikiGame.java
+++ WikiGame.java
@@ -15,12 +15,33 @@ public class WikiGame {
         String endLink = "";    // ending link, where the program is trying to get to
         maxDepth = 1;           // start this at 1 or 2, and if you get it going fast, increase
 
+        if (findLink(startLink, endLink, 0)) {
+            System.out.println("found it********************************************************************");
+            path.add(startLink);
+        } else {
+            System.out.println("did not found it********************************************************************");
+        }
+
     }
 
-    // recursion method, you'll probably want to rename it
-    // you may want this method to return something or take additional parameters
-    public void recurse(String startLink, String endLink, int depth) {
+    // recursion method
+    public boolean findLink(String startLink, String endLink, int depth) {
 
+        System.out.println("depth is: " + depth + ", link is: https://en.wikipedia.org" + startLink);
+
+        // BASE CASE
+        if () {
+
+        } else if () {
+
+        }
+
+        // GENERAL RECURSIVE CASE
+        else {
+
+        }
+
+        return false;
     }
-  
+
 }
